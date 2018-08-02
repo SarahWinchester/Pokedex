39d7321ac02c883af8c62db5e429d1ff25ab440a
SarahWinchester <indira_ruli@hotmail.com>
2018-08-02 04:19:04 -0500

    adding first JSON who contains pokemon id and name

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 //bring all IDs
 const pokeTemplate1 = document.getElementById('pokeTemplate1').innerHTML;
 const pokeTemplate2 = document.getElementById('pokeTemplate2').innerHTML;
+const pokeApiUrl = "https://pokeapi.co/api/v2/generation/1/"
 
-//searchInput
+$.getJSON(pokeApiUrl, function(data){
+    console.log(data);
+})
+    
