ff59dca59e2f35e1fff22709e63588d5942c0a39
SarahWinchester <indira_ruli@hotmail.com>
2018-08-02 03:31:12 -0500

    all design finished starting js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//bring all IDs
+const pokeTemplate1 = document.getElementById('pokeTemplate1').innerHTML;
+const pokeTemplate2 = document.getElementById('pokeTemplate2').innerHTML;
+
+//searchInput
