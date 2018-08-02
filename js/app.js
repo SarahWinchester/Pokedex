d68be371e3f60cc08e18675b033573d548238ef9
SarahWinchester <indira_ruli@hotmail.com>
2018-08-02 06:05:19 -0500

    trying to get a precised response from API

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
 //bring all IDs
-const pokeTemplate1 = document.getElementById('pokeTemplate1').innerHTML;
-const pokeTemplate2 = document.getElementById('pokeTemplate2').innerHTML;
-const pokeApiUrl = "https://pokeapi.co/api/v2/generation/1/"
+const pokeTemplate1 = $('#pokeTemplate1').innerHTML;
+const pokeTemplate2 = $('#pokeTemplate2').innerHTML;
+const pokemonInput = $('#findingPokemon')
+// const pokeApiUrlGen = "https://pokeapi.co/api/v2/generation/1/"
+const pokeApiUrlPokemon = "http://pokeapi.co/api/v2/pokemon/"+pokemonInput;
 
-$.getJSON(pokeApiUrl, function(data){
-    console.log(data);
-})
+function getPokemon() {
+    $.getJSON(pokeApiUrlPokemon, function(data){
+        console.log(data);
+    })
+}
+
+// $.getJSON(pokeApiUrl, function(data){
+//     console.log(data);
+// })
+//filling all templates
+// function getPokemon(e){
+//     for (const key in e) {
+//         let filled ="";
+//        console.log(key);
+//        let value = data[key];
+//        console.log(value);
+       
+//        filled += pokeTemplate1.replace("{{name}}", value.name)
+//        pokemonBox.innerHTML+=filled;
+//       }
+// }
     
+//input search
+$(document).ready(function(inputval) {
+    $('#searchButton').click(function(){
+        let searchTxt = pokemonInput.val;
+        console.log(searchTxt);
+        
+        // $.each(data.pokemon_species, function(){
+        //     if(pokemon_species.name == searchTxt){
+        //         const name = pokemon_species.name
+        //     }
+        // })
+    
+    })
+    getPokemon(inputval);
+});
