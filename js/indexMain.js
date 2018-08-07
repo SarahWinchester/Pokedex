e8d9b9888737fead1659ba1421ac72b468780bc3
SarahWinchester <indira_ruli@hotmail.com>
2018-08-06 22:35:33 -0500

    added splash , but im not sure if the carousel in search works

---
+++ js/indexMain.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    var url = 'views/main.html';
+    
+
+    $('body').fadeOut(0).fadeIn(650);
+
+    setTimeout(function(){
+        $('body').fadeOut(650, function(){
+            window.location = url;
+        });
+    }, 3000)
+});
